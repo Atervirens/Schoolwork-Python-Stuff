0fc1adce59e01fbec1d82459fd299a09a1d7341e
98JP11 <98jp11@LICT232.bourne-grammar.lincs.sch.uk>
2015-09-18 13:46:32 +0100

    program template that allows for decimal inputs and prevents invalid inputs

---
+++ Program base.py
@@ -0,0 +1,15 @@
+value=0
+carryon=True
+while True: #insert programming below Try
+
+except ValueError: #Checks whether or not inputs are valid
+    print("Insert error message here)
+
+carry=input("Insert true/false query. yes/no")
+
+if carry=="yes": #works with any string value
+    carryon=True
+else:
+    print("Insert termination message")
+    carryon=False
+    break #completely terminates program if termination option is chosen
