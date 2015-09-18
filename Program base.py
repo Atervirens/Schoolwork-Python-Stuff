80cefb475f1b43725963c997d24086aff27a9f1d
98JP11 <98jp11@LICT232.bourne-grammar.lincs.sch.uk>
2015-09-18 14:29:02 +0100

    program base that checks for input errors while allowing float values

--- Program base.py
+++ Program base.py
@@ -1,15 +1,16 @@
 value=0
 carryon=True
 while True: #insert programming below Try
+    try:
 
-except ValueError: #Checks whether or not inputs are valid
-    print("Insert error message here)
+    except ValueError: #Checks whether or not inputs are valid
+        print("Insert error message here)
 
-carry=input("Insert true/false query. yes/no")
+    carry=input("Insert true/false query. yes/no")
 
-if carry=="yes": #works with any string value
-    carryon=True
-else:
-    print("Insert termination message")
-    carryon=False
-    break #completely terminates program if termination option is chosen
+    if carry=="yes": #works with any string value
+        carryon=True
+    else:
+        print("Insert termination message")
+        carryon=False
+        break #completely terminates program if termination option is chosen
