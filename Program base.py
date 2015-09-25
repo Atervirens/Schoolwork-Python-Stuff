3554fe9ea206b50e384565e5b2bb226fb5bbbbac
98JP11 <98jp11@LICT232.bourne-grammar.lincs.sch.uk>
2015-09-25 11:16:07 +0100

    updating to use a function

--- Program base.py
+++ Program base.py
@@ -1,16 +1,26 @@
-value=0
-carryon=True
-while True: #insert programming below Try
+def inputcheck(value): #Function checks if the input is a number or not
     try:
+        value=float(value)
+        return True
+    except ValueError:
+        return False
+    
+carryon=True
+while True: #insert programming below while True
+    value=input("Input a value: ")
+    if inputcheck(value)==True:
+        value=float(value)
+        print(value)
+    else:
+        print("Invalid input.")
+        value=input("Input a value: ")
+    
 
-    except ValueError: #Checks whether or not inputs are valid
-        print("Insert error message here)
-
-    carry=input("Insert true/false query. yes/no")
+    carry=input("Insert true/false query. yes/no ")
 
     if carry=="yes": #works with any string value
         carryon=True
     else:
-        print("Insert termination message")
+        print("Insert termination message ")
         carryon=False
         break #completely terminates program if termination option is chosen
