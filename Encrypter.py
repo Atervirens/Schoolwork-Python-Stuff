36b358f1f2e8350dc79aead5eaec1a6dd13f0752
98JP11 <98jp11@LICT232.bourne-grammar.lincs.sch.uk>
2015-10-08 15:03:16 +0100

    update

--- Encrypter.py
+++ Encrypter.py
@@ -1,47 +1,47 @@
 alphabet='abcdefghijklmnopqrstuvwxyz '
 shifted='defghijklmnopqrstuvwxyz abc'
-carryon=True
-while True:
+alinput=['encrypt', 'decrypt', 'end']
+
+def checkinput(): #Function to check input for validity
     while True:
-        deci=input("Encrypt or decrypt? (encrypt/decrypt) ")
-        if deci=="encrypt":
-            
-            plain=input("Input message (don't capitalize or punctuate): ")
-
-            citext=''
-
-            for hold in plain: #caesar encryption program
-                pos=alphabet.index(hold)
-                citext=citext+shifted[pos]
-            print("Your message is now '", citext, "'.")
-            
-        elif deci=="decrypt":
-            
-            
-            citext=input("Type encrypted message (don't capitalize or punctuate): ")
-            plain=''
-            for pi in citext: #decryption program
-                hos=shifted.index(pi)
-                plain=plain+alphabet[hos]
-            print("Your message is '", plain, "'.")
-            
+        deci=input("Encrypt, decrypt or end? (encrypt/decrypt/end) ")
+        if deci in alinput:
+            return deci
         else:
             print("encrypt or decrypt only.")
-            
-            carryon=False
-        carry=input("Use again? encrypt/decrypt/no ")
-        if carry=="encrypt": #works with any string value
-            carryon=True
-            plain=input("Input message (don't capitalize or punctuate): ")
+           
+deci=input("Encrypt, decrypt or end? (encrypt/decrypt/end) ")
+carryon=True
+while carryon:
+    
+    if deci=="encrypt":
+        
+        plain=input("Input message (don't capitalize or punctuate): ")
 
-        elif carry=="decrypt":
-            carryon=True
-            citext=input("Type encrypted message (don't capitalize or punctuate): ")
-        elif carry=="no":
-            print("Terminating program.")
-            break 
+        citext=''
+
+        for hold in plain: #caesar encryption program
+            pos=alphabet.index(hold)
+            citext=citext+shifted[pos]
+        print("Your message is now '", citext, "'.")
+        deci=checkinput()
+        
+    if deci=="decrypt":
+        
+        
+        citext=input("Type encrypted message (don't capitalize or punctuate): ")
+        plain=''
+        for pi in citext: #decryption program
+            hos=shifted.index(pi)
+            plain=plain+alphabet[hos]
+        print("Your message is '", plain, "'.")
+        deci=checkinput()
+    if deci=="end":
+        print("Ending program")
+        carryon=False
+        break
+    
+        
+    
             
-        else:
-            print("encrypt, decrypt or no only.")
-            carry=input("Use again? encrypt/decrypt/no ")
 
