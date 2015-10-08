579e8972353370b2fede04c0abd6e3f1730f81be
98JP11 <98jp11@LICT232.bourne-grammar.lincs.sch.uk>
2015-10-08 15:16:00 +0100

    update

--- Encrypter.py
+++ Encrypter.py
@@ -8,33 +8,53 @@ def checkinput(): #Function to check input for validity
         if deci in alinput:
             return deci
         else:
-            print("encrypt or decrypt only.")
-           
+            print("encrypt, decrypt and end only.")
+def encrypt():
+    plain=input("Input message (don't capitalize or punctuate): ")
+
+    citext=''
+
+    for hold in plain: #caesar encryption program
+        pos=alphabet.index(hold)
+        citext=citext+shifted[pos]
+    print("Your message is now '", citext, "'.")
+def dencrypt():
+    citext=input("Input message (don't capitalize or punctuate): ")
+
+    plain=''
+
+    for pi in citext: #decryption program
+        hos=shifted.index(pi)
+        plain=plain+alphabet[hos]
+    print("Your message is now '", plain, "'.")
+keysize=26
+def keysizer(): #asks for keysizes and confirms that it's an integet between or equal to 1-26
+    while True:
+        inp=input("Input input between 1 and 26: ")
+        try:
+            inp=float(inp)
+            if inp>26 or inp<1:
+                
+              
+                print("Keysize doesn't fit")
+            else:
+                print("Your keysize is: ", inp)
+                return inp
+        except ValueError:
+            print("That's not a number")
 deci=input("Encrypt, decrypt or end? (encrypt/decrypt/end) ")
 carryon=True
 while carryon:
     
     if deci=="encrypt":
         
-        plain=input("Input message (don't capitalize or punctuate): ")
 
-        citext=''
-
-        for hold in plain: #caesar encryption program
-            pos=alphabet.index(hold)
-            citext=citext+shifted[pos]
-        print("Your message is now '", citext, "'.")
+        plain=encrypt()
         deci=checkinput()
         
     if deci=="decrypt":
         
-        
-        citext=input("Type encrypted message (don't capitalize or punctuate): ")
-        plain=''
-        for pi in citext: #decryption program
-            hos=shifted.index(pi)
-            plain=plain+alphabet[hos]
-        print("Your message is '", plain, "'.")
+        citext=dencrypt()
         deci=checkinput()
     if deci=="end":
         print("Ending program")
